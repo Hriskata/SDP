3a0d23433a52f811374ee11b796466687dbc588e
Hriskata <105062473+Hriskata@users.noreply.github.com>
2022-09-12 22:09:00 +0300

    Add files via upload

---
+++ 1.3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+void ageToDays(int age)
+{
+    unsigned int days = age * 365;
+    unsigned int hours = age * 365 * 24;
+    unsigned long int minutes = age * 365 * 24 * 60;
+    unsigned long long int seconds = age * 365 * 24 * 60 * 60;
+
+    std::cout << "Days: " << days << std::endl;
+    std::cout << "Hours: " << hours << std::endl;
+    std::cout << "Minutes: " << minutes << std::endl;
+    std::cout << "Seconds: " << seconds << std::endl;
+}
+
+int main()
+{
+    int age;
+    std::cout << "Enter your age : ";
+    std::cin >> age;
+    ageToDays(age);
+}
+
+
